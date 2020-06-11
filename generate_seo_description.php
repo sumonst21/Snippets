b9450ef83dcf27ee1ad57ac11fc907f252cd9d58
Md. Sumon Islam <sumonst21@gmail.com>
2020-06-12 05:48:35 +0600

    Create generate_seo_description.php

---
+++ generate_seo_description.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * truncate function
+ * Perfect SEO Description Generation
+ *
+ * @param [type] $text
+ * @param integer $chars
+ * @return void
+ */
+function truncate($text, $chars = 160) {
+    if (strlen($text) <= $chars) {
+        return $text;
+    }
+    $text = $text." ";
+    $text = substr($text,0,$chars);
+    $text = substr($text,0,strrpos($text,' '));
+    $text = $text."...";
+    return $text;
+}
+
+echo truncate('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
